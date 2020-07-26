af87738e8c32d1cf96171b3b177e1dd69cbc6d76
HuyNguyenAu <huynguyenau@gmail.com>
2020-07-26 17:25:15 +1000

    Moved set content to own method.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+/*jshint esversion: 6 */
+/* jshint browser: true */
+/*jslint devel: true */
+
+'use strict';
+
 window.addEventListener('load', onLoadEvent);
 window.addEventListener('hashchange', onHashChangeEvent);
 
@@ -6,63 +12,51 @@ function onHashChangeEvent(event) {
 }
 
 function onLoadEvent() {
-    // Clear out any existing content.
-    document.getElementById('content').innerHTML = '';
+    if (!document.location.hash) {
+        document.location.hash = '#home';
+    }
 
     switch (document.location.hash) {
-        case '':
-            document.location.hash = '#home';
-            break;
         case '#home':
             showHome();
             break;
         default:
-            showContent('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/html/' + document.location.hash.replace('#', '') + '.html');
+            get('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/html/' + document.location.hash.replace('#', '') + '.html');
             break;
     }
 }
 
 function showError(error) {
-    showContent('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/html/error.html', function () {
-        document.querySelector('.article-body').innerHTML += "<p>" + error + "<p>";
-    });
+    get('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/html/error.html')
+    .then((html) => showContent(html))
+    .then(() => document.querySelector('.article-body').innerHTML += "<p>" + error + "<p>");
 }
 
 function showHome() {
-    get('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/json/articles.json', function (json) {
-        JSON.parse(json).articles.forEach(article => {
-            get(article.url, function (html, url) {
-                document.getElementById('content').innerHTML += createArticle(html, url);
-            });
+    document.getElementById('content').innerHTML = '';
+
+    get('https://raw.githubusercontent.com/HuyNguyenAu/huynguyen/master/json/articles.json')
+        .then((json) => {
+            let jobs = [];
+
+            JSON.parse(json).articles.forEach(article => jobs.push(get(article.url)
+                .then((html) => createArticle(html, article.url))));
+
+            Promise.all(jobs).then((content) => showContent(content.join()));
         });
-    });
 }
 
-function showContent(url, callback) {
-    get(
-        url,
-        function (html, _) {
-            document.getElementById('content').innerHTML = html;
-
-            if (typeof (callback) === 'function') {
-                callback();
-            }
-        }
-    );
+function showContent(html) {
+    document.getElementById('content').innerHTML = html;
 }
 
-async function get(url, callback) {
+function get(url) {
     return fetch(url)
         .then((response) => {
             if (response.ok) {
                 return response.text();
             } else {
                 throw new Error(response.status + '. Failed to load ' + url);
-            }
-        })
-        .then((content) => {
-            if (typeof (callback) === 'function') {
-                callback(content, url);
             }
         })
         .catch((e) => {
@@ -76,10 +70,7 @@ function createArticle(html, url) {
     temp.innerHTML = html;
 
     let title = temp.querySelector('.article-title');
-    /* Add link to .article-title so when the user clicks on the title,
-   we can get that link to load the article into the content. */
-    title.setAttribute('link', url);
-    /* Wrap the header in a link so it give the user visual feedback of a link. */
+    /* Wrap the header in a link so it give the visual feedback of a link. */
     title.outerHTML = '<a class="article-title-link" href="#' + url.split('/').pop().replace('.html', '') + '">' + title.outerHTML + "</a>";
 
     let paragraphs = temp.querySelectorAll('.article-body p');
