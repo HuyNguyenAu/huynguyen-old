72063f84a0db92070cb7615f472f49a57b964f8a
HuyNguyenAu <huynguyenau@gmail.com>
2020-10-23 20:57:51 +1100

    Added grow to archive items.

--- build/build.py
+++ build/build.py
@@ -74,6 +74,7 @@ def buildContent(items, url):
     for article in items:
         card = article.html.find("div", {"class": "card"})
         card["class"].append("is-clickable")
+        card["class"].append("grow")
         card["url"] = article.url
 
         paragraphs = article.html.find("div", {"class": "card-content"}).find("div", {"class": "content"}, recursive=False).findAll("p")
